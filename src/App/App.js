d252778a5daaf5950a1ec049baa620de5cc320e8
Lilit <mkrtchyan.lilit.y@gmail.com>
2021-07-13 14:05:29 +0400

    Integrated sass

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header"></header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
