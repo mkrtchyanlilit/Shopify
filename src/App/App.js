b75eacdbc66e5fbfb10f95daa5a0e233d4265e08
Lilit <mkrtchyan.lilit.y@gmail.com>
2021-07-13 14:15:39 +0400

    Created home page

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,71 @@
-// import axios from "axios";
+import { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app__header"></header>
-      <main></main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    categories: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://ns-shopify.herokuapp.com/api/v1/categories"
+      );
+      this.setState({ categories: response.data });
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    // axios
+    //     .get("https://ns-shopify.herokuapp.com/api/v1/categories")
+    //     .then((response) => {
+    //         console.log(response);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err.message);
+    //     });
+    //? FETCH ASYNC REQUEST
+    // const response = fetch("https://ns-shopify.herokuapp.com/api/v1/categories");
+    // response
+    //     .then((res) => res.json())
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+  }
+
+  render() {
+    const { categories } = this.state;
+    console.log(categories);
+
+    const sortedCategories = categories.sort((a, b) => a.id - b.id);
+
+    return (
+      <div className="app">
+        <header className="app__header"></header>
+        <main className="app__main">
+          <ul className="categories">
+            {sortedCategories.map((category) => {
+              console.log(category);
+              return (
+                <li key={category._id} className="category">
+                  <div className="category__wrapper">
+                    <div
+                      className="image"
+                      style={{ backgroundImage: `url(${category.imageUrl})` }}
+                    />
+                    <div className="content">
+                      <div className="u-uppercase title">{category.title}</div>
+                      <div className="u-uppercase">shop now</div>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
